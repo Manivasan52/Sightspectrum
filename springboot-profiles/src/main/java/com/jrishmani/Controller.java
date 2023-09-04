78b7bbe95995745cd82adcd8281efd978de96339
unknown <you@example.com>
2023-09-04 16:51:28 +0530

    spring profiles project

---
+++ springboot-profiles/src/main/java/com/jrishmani/Controller.java
@@ -0,0 +1,17 @@
+package com.jrishmani;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	@GetMapping("/hello")
+public String sayHello() {
+	return dbUrl;
+}
+}
---
+++ springboot-profiles/src/test/java/com/jrishmani/SpringbootProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrishmani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
